be4439937c24e180efa64352cbe20a2052647cc0
unknown <nootherthanme65@gmail.com>
2022-02-10 15:32:32 +1300

    made changes to the looks of search bar according to tony's suggestions

--- assets/scripts/resource.js
+++ assets/scripts/resource.js
@@ -34,29 +34,13 @@ function resourceOnload() {
   });
   buttonsPressed = false;
   input.addEventListener('focus', () => {
-    console.log(document.activeElement);
-    console.log("hello"); //document.querySelector("autoComplete_list_1").removeAttribute('hidden');
-
     if (input.value !== "") {
       $('#autoComplete_list_1').removeAttr('hidden');
     } // Do your stuff
-
-  });
-  input.addEventListener('hover', (event, eleTest) => {
-    var rect = eleTest.getBoundingClientRect();
-    var x = event.clientX;
-
-    if (x < rect.x + rect.width && x > rect.x + rect.width - 55) {
-      // input.value = "";
-      // input.blur();
-      // input.focus();
-      // $('#autoComplete').keyup();
-      input.style.cursor = "pointer";
-    }
+    $('#autoComplete').keyup();
   });
   input.addEventListener('change', () => input.hasChanged = true);
   input.addEventListener('blur', () => {
-    console.log(document.activeElement);
     $('#autoComplete_list_1').attr('hidden', '""'); // Do your stuff
   });
 }
@@ -64,13 +48,29 @@ function resourceOnload() {
 function listClicky(thing) {
   document.getElementById("autoComplete").innerHTML = thing.textContent;
   document.getElementById("autoComplete").value = thing.textContent;
-  document.getElementById("autoComplete").outerText = thing.textContent;
-  console.log(document.getElementById("autoComplete").value);
-  return thing; //$('#autoComplete').keyup();
+  //$("#autoComplete").keyup();  
+  //document.getElementById("autoComplete").outerText = thing.textContent;
+  //return thing;
 } //starts when the buttons are pressed
 //searches if texts in the button is the same as the value of input/buttons
 //and shows the cards up
-
+function logKey() {
+  console.log("Hello");
+  var evt = new Event("look", {"bubbles":true, "cancelable":false});
+  document.dispatchEvent(evt);
+  input.value += `a`;
+  input.value +=` `;
+  input.value += `e`;
+  // input.value = "";
+  input.dispatchEvent(evt);
+  input.focus();
+  // var e = new KeyboardEvent('keyup',{'key':8,'which':8});// 32 is the keycode for the space bar
+  // //var a = new KeyboardEvent('keyup',{'keyCode':a,'which':65});
+  // input.dispatchEvent(e); 
+  // //input.dispatchEvent(a);
+  // input.blur(); 
+  // input.focus();
+}
 
 function buttonPressed(buttonValue) {
   if (buttonValue.getAttribute("buttonStatus") === "notPressed") {
@@ -87,9 +87,8 @@ function buttonPressed(buttonValue) {
 //if it is, then show every content
 
 
-function inputZero(valueLength) {
-  console.log(valueLength);
 
+function inputZero(valueLength) {
   if(valueLength > 0)
   {
       $('.magnifyingGlass').attr('class','noGlass');
@@ -105,27 +104,27 @@ function inputZero(valueLength) {
     $('.card-resources').css('visibility', 'visible');
     $('.card-resources').css('display', 'block');
     $('#autoComplete_list_1').attr('hidden', '""');
+    $(".autoComplete_wrapper").css("borderBottomRightRadius","50px");
   } else {
     input.style.borderBottomRightRadius = "0px";
     input.style.borderBottomLeftRadius = "0px";
-    
+    $(".autoComplete_wrapper").css("borderBottomRightRadius","0px");
+    let wrapping = document.querySelector(".autoComplete_wrapper");
+    let boxWidth = wrapping.offsetWidth
+    console.log(boxWidth);
+    $('#autoComplete_list_1').css("width",boxWidth);
   }
 } //deletes the value of autoComplete bar
 
 
-function clearAutoComplete(event, eleTest) {
-  var rect = eleTest.getBoundingClientRect();
-  var x = event.clientX;
-
-  if (document.querySelector('#autoComplete').className === 'noGlass') {
-    // &&(y<rect.y + rect.height && y < rect.y+rect.height-55)
-    if (x < rect.x + rect.width && x > rect.x + rect.width - 55) {
-      input.value = "";
-      input.blur();
-      $('#autoComplete').keyup();
-      input.focus();
+function clearAutoComplete(xButton) {
+    if(xButton.className=='noGlass')
+    {
+        document.querySelector('#autoComplete').value = ""; 
+        $('#autoComplete').keyup();
+        input.blur();
+        input.focus();
     }
-  }
 }
 
 function buttonSearching(buttonText) {
@@ -157,6 +156,7 @@ function buttonChange(textSearch, buttonsPressed) {
         content[i].style.display = "none";
       }
     }
+    input.foocus();
   } else {
     for (i = 0; i < buttonWrapperButtons.length; i++) {
       buttonWrapperButtons[i].blur();
